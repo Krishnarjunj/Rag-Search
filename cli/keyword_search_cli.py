7c6403810529a43b248d83e3a3abeddc7dd40349
Krishnarjunj <krishnarjunasta@gmail.com>
2026-02-07 12:09:35 +0530

    C: Added Tokenization, Punctuation filter and Sensitivity

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 from pathlib import Path
+import string
 
 
 def main() -> None:
@@ -14,33 +15,47 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    # FILE PATH 
+    # FILE PATH
     file_path_json = Path("~/Krish/RAG/rag-search-engine/data/movies.json").expanduser()
 
+    # Removing punctuations
+    Punctuations = string.punctuation
+    table = str.maketrans("", "", Punctuations)
 
-    # LOAD FILE 
+
+    # LOAD FILE
     with open(file_path_json, 'r') as f:
         data_json = json.load(f)
 
-    #QUERY 
-    seach_query = args.query
-
-
+    # QUERY
+    search_query = args.query.lower() #Lowercasing
+    search_query.translate(table) #Removing punctuations
+    tokenized_query = search_query.split() #tokenizing query
 
     match args.command:
         case "search":
-            print(f"Searching for: {seach_query}")
+            print(f"Searching for: {args.query}")
             pass
         case _:
             parser.print_help()
 
     # Adding movies to a list from data
-    result_list = []
+    movie_list = []
     for m in data_json["movies"]:
-        if seach_query in m["title"].split():
-            result_list.append(m["title"])
+        movie_list.append(m["title"])
 
-    #Printing Results
+    # Token to token comparison and adding to result
+    result_list = []
+    found = False
+    for query_token in tokenized_query:
+        for title in movie_list:
+            clean_title = title.lower().translate(table)
+            for title_token in clean_title.split():
+                if query_token in title_token:
+                    result_list.append(title)
+                    break
+
+    # Printing Results
     if len(result_list) > 5:
         for i in range(5):
             print(f"{i+1}. {result_list[i]}")
