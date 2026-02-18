7eeab7eead093462b0fcae97fe9dbffac9db4338
Krishnarjunj <krishnarjunasta@gmail.com>
2026-02-18 15:08:07 +0530

    added text embedding function

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -5,12 +5,35 @@ class SemanticSearch:
     def __init__(self):
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
 
+    def generate_embedding(self, text):
+        if text.strip("") == "":
+            raise ValueError(f"Empty string")
+
+        else:
+            input_list = []
+            input_list.append(text)
+
+            encoded_list = self.model.encode(input_list)
+
+            return encoded_list[0]
+
+
+def embed_text(text):
+    Obj = SemanticSearch()
+    embedding = Obj.generate_embedding(text)
+
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
+    return
 
 
 def verify_model():
     Obj = SemanticSearch()
     print(f"Model loaded: {Obj.model}")
     print(f"Max sequence length: {Obj.model.max_seq_length}")
+    return
 
 
 
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,13 +2,17 @@
 
 import argparse
 from lib.semantic_search import verify_model
+from lib.semantic_search import embed_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
 
     subparsers = parser.add_subparsers(dest="command")
 
-    subparsers.add_parser("verify")
+    verify_parser = subparsers.add_parser("verify")
+
+    embed_parser = subparsers.add_parser("embed_text")
+    embed_parser = embed_parser.add_argument("term", type=str)
 
     args = parser.parse_args()
 
@@ -16,11 +20,47 @@ def main():
         case "verify":
             verify()
 
+        case "embed_text":
+            term = args.term
+            embed_text(term)
+
+
         case _:
             parser.print_help()
 
 def verify():
     verify_model()
+
+
+'''
+def add_vectors(vec1, vec2):
+    if len(vec1) != len(vec2):
+        raise ValueError(f"Vectors of different lengths")
+
+    else:
+        res = []
+        for i in len(vec1):
+            res.append(vec1[i] + vec2[i])
+
+        return res
+
+    return
+
+def subtract_vectors(vec1, vec2):
+    if len(vec1) != len(vec2):
+        raise ValueError(f"Vectors of different lengths")
+
+    else:
+        res = []
+        for i in len(vec1):
+            res.append(vec2[i] - vec1[i])
+
+        return res
+
+    return
+
+'''
+
 
 if __name__ == "__main__":
     main()
